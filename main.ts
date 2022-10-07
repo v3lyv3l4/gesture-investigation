d94ed2fee430df4ac31d5d747ffe227d5c9cb11a
v3lyv3l4 <112582718+v3lyv3l4@users.noreply.github.com>
2022-10-06 19:48:01 -0700

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Surprised)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.House)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Duck)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Sad)
+})
